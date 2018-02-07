8248514dcccf52212d5c4938e2733366b05e664e
Otis Chou <altispec@gmail.com>
2018-02-07 20:39:02 +0800

    setup redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
 import App from './App'
+import reducer from './redux/rootReducer'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+let store = createStore(reducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 registerServiceWorker()
---
+++ client/src/redux/rootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+import bullet from './modules/bullet'
+
+const rootReducer = combineReducers({
+  bullet
+})
+
+export default rootReducer
